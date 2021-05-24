18b0932e3f4c785b51795763a9e2a0fa79a2dbc9
Mira.Fontan <mira.fontan@gmail.com>
2021-05-24 13:09:08 +0200

    Extract class Move from MoveList.kt to Move.kt

---
+++ app/src/main/java/cz/fontan/gomoku_gui/game/Move.kt
@@ -0,0 +1,27 @@
+package cz.fontan.gomoku_gui.game
+
+const val BOARD_SIZE: Int = 15
+
+enum class EnumMove {
+    Empty, Black, White, Wall
+}
+
+class Move(val x: Int = 0, val y: Int = 0, val type: EnumMove = EnumMove.Empty) {
+
+    init {
+        require(x >= 0)
+        require(y >= 0)
+        require(x < BOARD_SIZE)
+        require(y < BOARD_SIZE)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+
+        return (other is Move) && (x == other.x) && (y == other.y) && (type == other.type)
+    }
+
+    override fun hashCode(): Int {
+        return (x * BOARD_SIZE + y) * EnumMove.values().size + type.ordinal
+    }
+}
--- app/src/main/java/cz/fontan/gomoku_gui/game/MoveList.kt
+++ app/src/main/java/cz/fontan/gomoku_gui/game/MoveList.kt
@@ -1,31 +1,5 @@
 package cz.fontan.gomoku_gui.game
 
-const val BOARD_SIZE = 15
-
-enum class EnumMove {
-    Empty, Black, White, Wall
-}
-
-class Move(val x: Int = 0, val y: Int = 0, val type: EnumMove = EnumMove.Empty) {
-
-    init {
-        require(x >= 0)
-        require(y >= 0)
-        require(x < BOARD_SIZE)
-        require(y < BOARD_SIZE)
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-
-        return (other is Move) && (x == other.x) && (y == other.y) && (type == other.type)
-    }
-
-    override fun hashCode(): Int {
-        return (x * BOARD_SIZE + y) * EnumMove.values().size + type.ordinal
-    }
-}
-
 class MoveList : Iterator<Move> {
     private var moveList: ArrayList<Move> = arrayListOf()
     private var lastMoveIndex: Int = -1
