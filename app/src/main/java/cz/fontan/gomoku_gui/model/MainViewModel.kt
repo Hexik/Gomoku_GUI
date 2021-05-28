848618b2a527ad7743c7090de23cd98accfafb7e
Mira.Fontan <mira.fontan@gmail.com>
2021-05-28 10:28:42 +0200

    MainViewModel.kt MutableLiveData as private, non-mutable getters

--- app/src/main/java/cz/fontan/gomoku_gui/model/MainViewModel.kt
+++ app/src/main/java/cz/fontan/gomoku_gui/model/MainViewModel.kt
@@ -1,8 +1,10 @@
 package cz.fontan.gomoku_gui.model
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import cz.fontan.gomoku_gui.InterfaceMain
+import cz.fontan.gomoku_gui.NativeInterface
 import cz.fontan.gomoku_gui.game.BOARD_SIZE
 import cz.fontan.gomoku_gui.game.Game
 import cz.fontan.gomoku_gui.game.Move
@@ -10,44 +12,56 @@ import cz.fontan.gomoku_gui.game.Move
 class MainViewModel : ViewModel(), InterfaceMain {
     private val game = Game(BOARD_SIZE)
 
-    val isSearching = MutableLiveData<Boolean>()
-    val canRedo = MutableLiveData<Boolean>()
-    val canUndo = MutableLiveData<Boolean>()
+    private val _isSearching = MutableLiveData<Boolean>()
+    val isSearching: LiveData<Boolean>
+        get() = _isSearching
+
+    private val _canRedo = MutableLiveData<Boolean>()
+    val canRedo: LiveData<Boolean>
+        get() = _canRedo
+
+    private val _canUndo = MutableLiveData<Boolean>()
+    val canUndo: LiveData<Boolean>
+        get() = _canUndo
+
+    val fromBrain: MutableLiveData<ArrayList<String>> = MutableLiveData<ArrayList<String>>()
+
 
     init {
-        isSearching.value = false
-        canRedo.value = false
-        canUndo.value = false
+        _isSearching.value = false
+        _canRedo.value = false
+        _canUndo.value = false
     }
 
     fun startSearch() {
-        isSearching.value = true
-        canUndo.value = false
-        canRedo.value = false
+        _isSearching.value = true
+        _canUndo.value = false
+        _canRedo.value = false
+        NativeInterface.writeToBrain(game.toBoard(true))
     }
 
     fun stopSearch() {
-        isSearching.value = false
-        canUndo.value = game.canUndo()
-        canRedo.value = game.canRedo()
+        _isSearching.value = false
+        _canUndo.value = game.canUndo()
+        _canRedo.value = game.canRedo()
     }
 
     fun undoMove() {
         game.undoMove()
-        canUndo.value = game.canUndo()
-        canRedo.value = game.canRedo()
+        _canUndo.value = game.canUndo()
+        _canRedo.value = game.canRedo()
     }
 
     fun redoMove() {
         game.redoMove()
-        canUndo.value = game.canUndo()
-        canRedo.value = game.canRedo()
+        _canUndo.value = game.canUndo()
+        _canRedo.value = game.canRedo()
     }
 
     fun newGame() {
         game.newGame()
-        canUndo.value = game.canUndo()
-        canRedo.value = game.canRedo()
+        _canUndo.value = game.canUndo()
+        _canRedo.value = game.canRedo()
     }
 
     override fun canMakeMove(move: Move): Boolean {
@@ -56,8 +70,8 @@ class MainViewModel : ViewModel(), InterfaceMain {
 
     override fun makeMove(move: Move) {
         game.makeMove(move)
-        canUndo.value = isSearching.value == false && game.canUndo()
-        canRedo.value = isSearching.value == false && game.canRedo()
+        _canUndo.value = isSearching.value == false && game.canUndo()
+        _canRedo.value = isSearching.value == false && game.canRedo()
     }
 
     override fun moveCount(): Int {
@@ -69,7 +83,7 @@ class MainViewModel : ViewModel(), InterfaceMain {
     }
 
     override fun isSearching(): Boolean {
-        return game.searchMode
+        return isSearching.value == true
     }
 
 }
