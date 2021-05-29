261d9299392659bbb9fdde5adab29f67dc425f70
Mira.Fontan <mira.fontan@gmail.com>
2021-05-29 18:35:23 +0200

    ConsumableValue.kt data variable as private

--- app/src/main/java/cz/fontan/gomoku_gui/model/ConsumableValue.kt
+++ app/src/main/java/cz/fontan/gomoku_gui/model/ConsumableValue.kt
@@ -2,7 +2,7 @@ package cz.fontan.gomoku_gui.model
 
 import java.util.concurrent.atomic.AtomicBoolean
 
-class ConsumableValue<T>(val data: T) {
+class ConsumableValue<T>(private val data: T) {
 
     private val consumed = AtomicBoolean(false)
 
