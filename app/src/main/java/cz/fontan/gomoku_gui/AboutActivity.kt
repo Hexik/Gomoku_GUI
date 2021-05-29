9e33e3f0d364188cec6f07ca26fe449c6c57bf07
Mira.Fontan <mira.fontan@gmail.com>
2021-05-29 12:27:21 +0200

    AboutActivity.kt and activity_about.xml support ScrollView, usable for landscape functionality

--- app/src/main/java/cz/fontan/gomoku_gui/AboutActivity.kt
+++ app/src/main/java/cz/fontan/gomoku_gui/AboutActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class AboutActivity : AppCompatActivity() {
+class AboutActivity : AppCompatActivity(R.layout.activity_about) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
 
         // Build date from BuildConfig
         findViewById<TextView>(R.id.textViewAboutBuildDateValue).text = BuildConfig.BUILD_TIME
