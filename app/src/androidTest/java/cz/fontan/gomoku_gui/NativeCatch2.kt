19795f05fe7309ba32e55602bce3f29a178a3b9c
Mira.Fontan <mira.fontan@gmail.com>
2021-06-13 14:36:00 +0200

    NativeCatch2.kt load library only one time per test suite

--- app/src/androidTest/java/cz/fontan/gomoku_gui/NativeCatch2.kt
+++ app/src/androidTest/java/cz/fontan/gomoku_gui/NativeCatch2.kt
@@ -1,18 +1,21 @@
 package cz.fontan.gomoku_gui
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class NativeCatch2 {
-    @Before
-    fun init() {
-        try {
-            System.loadLibrary("native-lib")
-        } catch (e: UnsatisfiedLinkError) {
-            // log the error or track it in analytics
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
+            try {
+                System.loadLibrary("native-lib")
+            } catch (e: UnsatisfiedLinkError) {
+                // log the error or track it in analytics
+            }
         }
     }
 
