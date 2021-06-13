09b2335d7b794f13d20ac84a06335764025805ad
Mira.Fontan <mira.fontan@gmail.com>
2021-06-13 10:30:31 +0200

    Add instrumented test NativeCatch2

---
+++ app/src/androidTest/java/cz/fontan/gomoku_gui/NativeCatch2.kt
@@ -0,0 +1,23 @@
+package cz.fontan.gomoku_gui
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NativeCatch2 {
+    @Before
+    fun init() {
+        try {
+            System.loadLibrary("native-lib")
+        } catch (e: UnsatisfiedLinkError) {
+            // log the error or track it in analytics
+        }
+    }
+
+    @Test
+    fun basic() {
+        assert(0 == NativeInterface.runTest(""))
+    }
+}
