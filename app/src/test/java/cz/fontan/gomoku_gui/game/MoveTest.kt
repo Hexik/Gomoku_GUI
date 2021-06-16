de1daefd145cfe4bc8411b6094e489c3874dc667
Mira.Fontan <mira.fontan@gmail.com>
2021-06-16 17:52:58 +0200

    MoveTest.kt add wrong coordinates test

--- app/src/test/java/cz/fontan/gomoku_gui/game/MoveTest.kt
+++ app/src/test/java/cz/fontan/gomoku_gui/game/MoveTest.kt
@@ -8,4 +8,14 @@ class MoveTest {
         val m = Move()
         assert(m.hashCode() == 0)
     }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun wrong_coordinates_negative() {
+        Move(-1, -1)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun wrong_coordinates_too_big() {
+        Move(BOARD_SIZE_MAX, BOARD_SIZE_MAX)
+    }
 }
