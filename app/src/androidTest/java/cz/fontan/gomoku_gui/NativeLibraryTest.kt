2eb0254ade391ad4e89d142a8a90536182233fe8
Mira.Fontan <mira.fontan@gmail.com>
2021-06-19 08:46:30 +0200

    NativeLibraryTest.kt renamed from NativeLibrary.kt

--- app/src/androidTest/java/cz/fontan/gomoku_gui/NativeLibraryTest.kt
+++ app/src/androidTest/java/cz/fontan/gomoku_gui/NativeLibraryTest.kt
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class NativeLibrary {
+class NativeLibraryTest {
     companion object {
         @BeforeClass
         @JvmStatic
