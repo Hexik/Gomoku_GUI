437ca6e79303f7f30d13e28e97f2109ce0893b4d
Mira.Fontan <mira.fontan@gmail.com>
2021-06-19 08:39:43 +0200

    CountingIdlingResourceSingleton.kt add KDoc comments

--- app/src/main/java/cz/fontan/gomoku_gui/CountingIdlingResourceSingleton.kt
+++ app/src/main/java/cz/fontan/gomoku_gui/CountingIdlingResourceSingleton.kt
@@ -2,16 +2,28 @@ package cz.fontan.gomoku_gui
 
 import androidx.test.espresso.idling.CountingIdlingResource
 
+/**
+ * Idling resource limited to one instance
+ */
 object CountingIdlingResourceSingleton {
 
     private const val RESOURCE = "GLOBAL"
 
-    val countingIdlingResource = CountingIdlingResource(RESOURCE)
+    /**
+     * Idling resource instance
+     */
+    val countingIdlingResource: CountingIdlingResource = CountingIdlingResource(RESOURCE)
 
+    /**
+     * Increase usage count
+     */
     fun increment() {
         countingIdlingResource.increment()
     }
 
+    /**
+     * Decrease usage count
+     */
     fun decrement() {
         if (!countingIdlingResource.isIdleNow) {
             countingIdlingResource.decrement()
